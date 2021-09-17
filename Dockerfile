0389fb770fa052487b64ff5ae0ecb89e2138ab38
Md Mamunur Rashid <charlicoder@gmail.com>
2021-09-17 23:50:38 +0600

    Added django project to docker

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ LABEL maintainer="charlicoder.com"
 ENV PYTHONUNBUFFERED 1
 
 COPY ./requirements.txt ./requirements.txt
-COPY ./app /app 
+COPY ./app /app
 
 WORKDIR /app
 EXPOSE 8000
@@ -16,7 +16,12 @@ RUN python -m venv /py && \
         build-base postgresql-dev musl-dev && \
     /py/bin/pip install -r /requirements.txt && \
     apk del .tmp-deps && \
-    adduser --disabled-password --no-create-home app
+    adduser --disabled-password --no-create-home app && \
+    mkdir -p /vol/web/static && \
+    mkdir -p /vol/web/media && \
+    chown -R app:app /vol && \
+    chmod -R 755 /vol 
+
 
 ENV PATH="/py/bin:$PATH"
 
--- app/blog/management/commands/wait_for_db.py
+++ app/blog/management/commands/wait_for_db.py
@@ -1,6 +1,7 @@
 import time
 
-from psycopg2 imoprt OperationalError as Psycopg2OpError
+from psycopg2 import OperationalError as Psycopg2OpError
+
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
@@ -12,7 +13,7 @@ class Command(BaseCommand):
         db_up = False
         while db_up is False:
             try:
-                self.check(databases['default'])
+                self.check(databases=['default'])
                 db_up = True
             except (Psycopg2OpError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
